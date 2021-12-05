378948c135cfc24d64b77e6f39cf945e55e0ea90
Henrique-DS <henrique.q.paula@gmail.com>
2021-12-05 12:33:05 -0300

    Ajustes

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
-    $conexao = new mysqli("172.16.20.100","ibus_app","Ibus@pp3467");
+    //$conexao = new mysqli("172.16.20.100","ibus_app","Ibus@pp3467");
+    $conexao = new mysqli("201.62.65.6","ibus_app","Ibus@pp3467", "ibus_app");
     $conexao->set_charset("UTF8");
+
     if($conexao->connect_error){
         die("Falha ao conectar:".$conexao->connect_error);
-    }if(!$conexao->select_db("ibus_app")){
-        die("O Banco de dados não existe");
     }
 ?>
--- tb_usuario/cadastro.php
+++ tb_usuario/cadastro.php
@@ -1,46 +1,34 @@
 <?php
-
+    
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
         require("../conexao.php");
         $postdata = file_get_contents("php://input");
         $request = json_decode($postdata);
         
-        $nome_usuario = $request->nome_usuario;
-        $email_usuario = $request->email_usuario;
-        $senha_usuario = $request->senha_usuario;
-        $desc_usuario = $request->desc_usuario;
-        $dtNasc_usuario = $request->dtNasc_usuario;
-    
-        $sql = "
-            insert into 
-                tb_usuario 
-                (
-                    nome_usuario, 
+        $id_usuario = $request->id;
+        $nome_usuario = $request->nome;
+        $email_usuario = $request->email;
+        $senha_usuario = $request->senha;
+
+        $sql = "INSERT INTO tb_usuario 
+                    (nome_usuario, 
                     email_usuario, 
                     senha_usuario, 
                     desc_usuario, 
-                    dtNasc_usuario,
-                    config_usuario,
-                    localizacao_usuario
-                ) 
-                value
-                (
-                    '$nome_usuario', 
+                    dtNasc_usuario) 
+                VALUES
+                    ('$nome_usuario', 
                     '$email_usuario', 
                     '$senha_usuario', 
-                    '$desc_usuario', 
-                    '$dtNasc_usuario',
-                    'configurado',
-                    'aaaa'
-                )";
+                    'Descrição', 
+                    '1111-11-11')";
 
         $status = mysqli_query($conexao, $sql);
         
         if ($status) {
             http_response_code(201);
-            $data = ["mensagem" => "Inserido com sucesso"];
-            echo json_encode($data);
+            echo json_encode(["mensagem" => "Usuário cadastrado com sucesso."]);
         } else {
             header("HTTP/1.1 500 Erro no SQL");
             echo json_encode(["erro" => "Erro ao Inserir " . $conexao->error]);
